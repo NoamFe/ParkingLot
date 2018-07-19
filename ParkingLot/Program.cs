dfec45d0542af067e5133f3fbc4aa72bf461c105
Fein <Noam.Fein@aristocrat.com>
2018-07-19 10:36:39 -0500

    update package

--- ParkingLot/Program.cs
+++ ParkingLot/Program.cs
@@ -2,10 +2,8 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
-using UnitTestHelper;
+using NFSystems.Extensions;
 
 namespace ParkingLot
 {
